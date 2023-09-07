f6668f3e54cdb85d14aca12b0ae6c6ecd4d1dfa7
c-chukwunalu <profchukz@gmail.com>
2023-09-07 08:40:11 -0700

    my commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,47 +2,30 @@
 #include <stdlib.h>
 
 /**
-  * string_nconcat - Concatenates two strings
-  * @s1: string 1
-  * @s2: string 2
-  * @n: bytes number of s2 to concat to s1
+  * _calloc - Allocates memory for an array, using malloc
+  * @nmemb: number of elements
+  * @size: byte size of array elements
   *
-  * Return: pointer to newly allocated space in mem containing the concat str
+  * Return: pointer to alloc memory or NULL on failure
   */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	unsigned int i = 0, j = 0, k = 0;
+	unsigned int i = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (*(s2 + i) != '\0')
-		i++;
-	if (n > i)
-		n = i;
-	i = 0;
-	while (*(s1 + i) != '\0')
-		i++;
-	n += i;
-	ptr = malloc(sizeof(char) * (n + 1));
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	for (j = 0; j < n; j++)
+	for (i = 0; i < nmemb * size; i++)
 	{
-		if (j < i)
-			*(ptr + j) = *(s1 + j);
-		else
-		{
-			*(ptr + j) = *(s2 + k);
-			k++;
-		}
+		ptr[i] = 0;
 	}
-	*(ptr + j) = '\0';
+
 	return (ptr);
 }
